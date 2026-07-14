2bcc96404ee65b246d79b4ae135c282dcbb5d43a
trinkse61538 <32486887+trinkse61538@users.noreply.github.com>
2026-07-14 10:55:35 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/**
+ * Dán URL Apps Script Web App kết thúc bằng /exec vào giữa dấu nháy.
+ * Ví dụ:
+ * window.N0C_REPORT_API_URL =
+ *   'https://script.google.com/macros/s/ABC123/exec';
+ */
+window.N0C_REPORT_API_URL = 'https://script.google.com/macros/s/AKfycbwYDXNRDajtG8BcOVQuYH8VwJ1D22lVPluIy7dYRp652s7xX_R9G6VMP6JsxSwWFA673A/exec';
