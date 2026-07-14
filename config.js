450bfc44752686f336207e7fe388863f2143dd89
trinkse61538 <32486887+trinkse61538@users.noreply.github.com>
2026-07-14 13:22:17 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * Dán Apps Script Web App URL kết thúc bằng /exec.
  * Không dán Meta Access Token vào file này.
  */
-window.N0C_REPORT_API_URL = 'https://script.google.com/macros/s/AKfycbxplxzs1ITEmpPuXtGXoHZxOrz_XuzwOODdbwLvyBVQVuHfox9vy8ruETRlDMFcslatmQ/exec';
+window.N0C_REPORT_API_URL = 'https://script.google.com/macros/s/AKfycbx6aDlRx1vvUdj8PrTzOq5iWkDAETg4Us4qtb5vZ5sd5CmHm8Ev7Is-9y-RVeM_CQ4wWQ/exec';
