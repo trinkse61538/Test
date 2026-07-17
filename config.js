0e5fd43cdbec7c8ef4936af1972ce34cf11ff725
trinkse61538 <32486887+trinkse61538@users.noreply.github.com>
2026-07-17 09:42:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * Dán Apps Script Web App URL kết thúc bằng /exec.
  * Không dán Meta Access Token vào file này.
  */
-window.N0C_REPORT_API_URL = 'https://script.google.com/macros/s/AKfycbyEiD_s0cB7HCN_o1-nQTlAXNL6xCFfmS8RECfj4jtbel9vDUVMuCEdKE_-Oczp6hHikw/exec';
+window.N0C_REPORT_API_URL = 'https://script.google.com/macros/s/AKfycbwTC9I5faC-_kBuDF-CRiaaPlndvykxsAgdweHUYARVmX0OE6sQ12HkATSt24JOE8lJog/exec';
